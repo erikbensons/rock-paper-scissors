95b6825d696d3ab4c72a9b08b712cce9b0c7c351
Erik Benson <benson.erik02@gmail.com>
2024-10-01 17:15:23 +0200

    Code function that plays one round

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let humanScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
+
+/*for (let roundsLeft = 0; roundsLeft < 5; roundsLeft++) {
+    getComputerChoice();
+}*/
 
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3); // Gives value of 0, 1 or 2
@@ -19,16 +24,53 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    let humanChoice = prompt("Choose rock, paper or scissors: ");
+    let humanChoice = (prompt("Choose rock, paper or scissors: ")).toLowerCase();
+    return humanChoice;
 
-    if (humanChoice === ("rock" || "paper" || "scissors")) {
-        return humanChoice;
+    /*if (humanChoice == (("rock") || ("paper") || ("scissors"))) {
+
     }
     else {
         console.log("You did not choose, rock paper or scissors!")
-        getHumanChoice();
-    }
+    }*/
 }
 
-console.log("The computer's choice is: " + getComputerChoice());
-console.log("The user's choice is: " + getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    console.log("Welcome to round " + roundNumber);
+
+    if (humanChoice == 'rock' && computerChoice == 'scissors') {
+        humanScore++;
+        console.log("You chose " + humanChoice + " which beats the computer's choice " + computerChoice + ". You win!")
+        console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer");
+
+        console.log("- - - - - - - - - - - - - - - - - - - - - -")
+    } else if (humanChoice == 'paper' && computerChoice == 'rock') {
+        humanScore++;
+        console.log("You chose " + humanChoice + " which beats the computer's choice " + computerChoice + ". You win!")
+        console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer");
+
+        console.log("- - - - - - - - - - - - - - - - - - - - - -")
+    } else if (humanChoice == 'scissors' && computerChoice == 'paper') {
+        humanScore++;
+        console.log("You chose " + humanChoice + " which beats the computer's choice " + computerChoice + ". You win!")
+        console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer");
+
+        console.log("- - - - - - - - - - - - - - - - - - - - - -")
+    } else if (humanChoice ==  computerChoice) {
+        console.log("You chose " + humanChoice + " which is the same as the computer's choice " + computerChoice + ". You tie!")
+        console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer");
+
+        console.log("- - - - - - - - - - - - - - - - - - - - - -")
+    } else {
+        computerScore++;
+        console.log("You chose " + humanChoice + " which loses to the computer's choice " + computerChoice + ". You lose!")
+        console.log("The score is: Human " + humanScore + " - " + computerScore + " Computer");
+
+        console.log("- - - - - - - - - - - - - - - - - - - - - -")
+        
+    }
+
+    roundNumber++;
+}
+
+playRound(getHumanChoice(), getComputerChoice());
