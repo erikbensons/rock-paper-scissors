0a5a36b445006925d7c3126fbd2d68267fc28a9e
Erik Benson <benson.erik02@gmail.com>
2024-10-01 16:47:20 +0200

    Make computer make a choice

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello Elsa, you're so cool!");
+function getComputerChoice() {
+    let computerNumber = Math.floor(Math.random() * 3); // Gives value of 0, 1 or 2
+    let computerChoice;
+
+    if (computerNumber === 0) {
+        computerChoice = "rock";
+    }
+    else if (computerNumber === 1) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
