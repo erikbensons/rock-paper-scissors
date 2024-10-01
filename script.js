bee7190c7aed9034a44546634d4c436a8997af8e
Erik Benson <benson.erik02@gmail.com>
2024-10-01 16:56:33 +0200

    Add variables for score

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let computerNumber = Math.floor(Math.random() * 3); // Gives value of 0, 1 or 2
     let computerChoice;
